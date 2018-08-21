d3c6f8b1d99921afe5fb096cd192ffeec2c49b39
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-21 20:15:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
 
   def self.new_from_db(row)
     #binding.pry
-    new_student = self.new(name, grade, id)
+    new_student = self.new(row[1], row[2], row[0])
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
