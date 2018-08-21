c2962634e1adfaec5a52038d01cb0bd5cc796187
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-21 19:56:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,6 +56,14 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
