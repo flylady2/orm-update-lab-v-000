ec05a5a8dc1d050c46fc1200946bd20c64da5ef8
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-21 20:00:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
-  def self.new_from_db()
+  def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
