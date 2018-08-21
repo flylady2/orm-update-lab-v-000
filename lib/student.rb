0124cd33c22ecda0c9017e84b92141d228300389
Leslie Stevens <flylady@mail.utexas.edu>
2018-08-21 20:16:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,8 @@ require_relative "../config/environment.rb"
 require 'pry'
 
 class Student
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :name, :grade, :id
+  #attr_reader :id
 
   def initialize(name, grade, id =nil)
     @name = name
